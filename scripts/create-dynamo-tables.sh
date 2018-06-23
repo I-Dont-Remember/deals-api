010072015e3770a95b13cacd10baf3118b8b9a9b
Kevin Quinn <kpquinn2@wisc.edu>
2018-06-23 17:53:47 -0500

    create-tables: Add more options and better parsing

--- scripts/create-dynamo-tables.sh
+++ scripts/create-dynamo-tables.sh
@@ -1,19 +1,43 @@
 #! /bin/bash
 command="awslocal"
-if [ $# -ne 0 ]; then
-    if [ $1 = "--remote" ]; then
-        command="aws"
-    fi
-fi
+dealsTable="devDeals"
+locationsTable="devLocations"
 
-echo "Creating Deals table..."
-$command dynamodb create-table --table-name Deals \
+while getopts ":d:l:r" opt; do
+    case $opt in
+        d)
+            dealsTable=$OPTARG
+            echo "-d deals table $dealsTable"
+        ;;
+        l)
+            locationsTable=$OPTARG
+            echo "-l Location table $locationsTable"
+        ;;
+        r)
+            command="aws"
+            echo "-r remote $command"
+        ;;
+        \?)
+            echo "Invalid Option: -$OPTARG"
+            exit 1
+        ;;
+        :)
+            echo "Option -$OPTARG requires an argument."
+            exit 1
+    esac
+done
+
+# Understanding what to do for tables https://www.dynamodbguide.com/working-with-multiple-items
+# "Give me all of the deals(Range) from a particular location(Hash)"
+# This then doesn't work,because primary id is now location, which has many so you only get 1 deal per location
+echo "Creating $dealsTable table..."
+$command dynamodb create-table --table-name "$dealsTable" \
     --attribute-definitions AttributeName=id,AttributeType=S \
     --key-schema AttributeName=id,KeyType=HASH \
     --provisioned-throughput ReadCapacityUnits=1,WriteCapacityUnits=1
 
-echo "Creating Locations table..."
-$command dynamodb create-table --table-name Locations \
+echo "Creating $locationsTable table..."
+$command dynamodb create-table --table-name "$locationsTable" \
     --attribute-definitions AttributeName=id,AttributeType=S \
     --key-schema AttributeName=id,KeyType=HASH \
     --provisioned-throughput ReadCapacityUnits=1,WriteCapacityUnits=1
